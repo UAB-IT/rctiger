e7559208557ebf383337e365d9005456ca610490
jgordini <jgordini@uab.edu>
2023-11-05 22:19:15 -0600

    start

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -69,11 +69,11 @@
 			</p>
 			<div class="mt-10 flex items-center gap-x-6">
 				<a
-					href="#"
+					href="https://rc.uab.edu"
 					class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
 					>Get started</a
 				>
-				<a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+				<a href="https://docs.rc.uab.edu" class="text-sm font-semibold leading-6 text-gray-900"
 					>Learn more <span aria-hidden="true">→</span></a
 				>
 			</div>
