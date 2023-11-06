37417b7944fc10945abae66f07b0d0ec0ab7dfa9
jgordini <jgordini@uab.edu>
2023-11-05 21:54:32 -0600

    faq updates

---
+++ src/lib/Questions.svelte
@@ -0,0 +1,38 @@
+<div class="bg-white">
+    <div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
+      <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+      <p class="mt-6 max-w-2xl text-base leading-7 text-gray-600">Have a different question and can’t find the answer you’re looking for? Reach out to our support team by <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">sending us an email</a> and we’ll get back to you as soon as we can.</p>
+      <div class="mt-20">
+        <dl class="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:gap-x-10">
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">What is Cheaha</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">Cheaha is a supercomputer located at the University of Alabama Birmingham (UAB). It is currently the largest and fastest supercomputer in the state of Alabama. Cheaha offers high performance computing resources to researchers across many disciplines and institutions. It can handle data-intensive, computational research projects that would not be feasible on a typical desktop or lab computer.</dd>
+          </div>
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">How do I get access to Cheaha?</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">To gain access to Cheaha, you will need to request an account through the UAB IT Research Computing group. Accounts are available to faculty, staff, and students at UAB as well as external collaborators. The account request process involves submitting basic information about your research project and intended Cheaha usage. This helps determine the appropriate resource allocation and storage quotas for your work.</dd>
+          </div>
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">How can I run my research software on Cheaha?</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">Cheaha supports a wide variety of scientific research software via Environment Modules. This includes programming languages like R, Python, MATLAB, development tools, math libraries, deep learning frameworks, and domain-specific packages for chemistry, biology, engineering, and more. Before running a program, you will need to load the required modules into your environment.</dd>
+          </div>
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">How can I collaborate with researchers outside UAB?</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">Cheaha accounts can be created for external collaborators from other universities, companies, or research institutes to facilitate sharing data and compute resources. The process involves the UAB primary investigator submitting a request through the XIA system (External Identity and Access). This allows creating a unique Cheaha identity for the external user while still tracking their affiliation and resource usage.</dd>
+          </div>
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">What are some tips to help use Cheaha efficiently?</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">To make the most of Cheaha, become familiar with Linux commands related to job submission and queuing.</dd>
+          </div>
+          <div>
+            <dt class="text-base font-semibold leading-7 text-gray-900">How can I get help with Cheaha?</dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">If you need assistance using Cheaha or have questions about the system, the UAB IT Research Computing group provides support through multiple channels.</dd>
+          </div>
+         
+  
+          <!-- More questions... -->
+        </dl>
+      </div>
+    </div>
+  </div>
+  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Header from '$lib/Banner.svelte';
 	import Features from '$lib/Features.svelte';
 	import FAQ from '$lib/FAQ.svelte';
+	import Questions from '$lib/Questions.svelte';
 	import News from '$lib/News.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import '../app.css';
@@ -13,7 +14,7 @@
 	<Nav />
 	<Header />
 	<Features />
-	<FAQ />
+	<Questions />
 	<Footer />
 	<slot />
 </main>
