721266f40b2ec96da84abdc235442c1d3d8390aa
jgordini <jgordini@uab.edu>
2023-11-05 22:01:24 -0600

    docs to mobile menu

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -384,6 +384,11 @@
 								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>About</a
 							>
+							<a
+								href="#"
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>Docs</a
+							>
 						</div>
 						<div class="py-6">
 							<a
