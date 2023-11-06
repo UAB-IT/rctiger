da48385f4f52a795d37f954c088f5cc2bdedad92
jgordini <jgordini@uab.edu>
2023-11-05 20:43:32 -0600

    Nav to Menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Nav from '$lib/Nav.svelte';
+	import Nav from '$lib/Menu.svelte';
 	import Header from '$lib/Header.svelte';
 	import Features from '$lib/Features.svelte';
 	import FAQ from '$lib/FAQ.svelte';
